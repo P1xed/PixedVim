ef8a2a63aa86a17e66c77973224b41da6e59dcb4
_Pixed_ <140726085+P1xed@users.noreply.github.com>
2025-05-03 23:49:15 +0800

    files
    ...

---
+++ lua/plugins/LSP.lua
@@ -0,0 +1,146 @@
+return {
+
+	{
+
+		"williamboman/mason.nvim",
+		dependencies = {
+			"williamboman/mason-lspconfig.nvim",
+			"neovim/nvim-lspconfig",
+		},
+		event = "VeryLazy",
+		opts = {},
+		config = function(_, opts)
+			require("mason").setup(opts)
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"clangd",
+					"cmake",
+				},
+				automatic_installation = true,
+			})
+			require("lspconfig")["lua_ls"].setup({
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+					},
+				},
+			})
+			require("lspconfig")["clangd"].setup({
+				cmd = {
+					"clangd",
+					"--header-insertion=never",
+				},
+			})
+			vim.cmd("LspStart")
+			vim.diagnostic.config({
+				virtual_text = true,
+			})
+		end,
+	},
+
+	{
+		"saghen/blink.cmp",
+		dependencies = { "rafamadriz/friendly-snippets" },
+		version = "*",
+		opts = {
+			keymap = { preset = "super-tab" },
+			completion = { documentation = { auto_show = true } },
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+			},
+			cmdline = {
+				sources = function()
+					local cmd_type = vim.fn.getcmdtype()
+					if cmd_type == "/" then
+						return { "buffer" }
+					end
+					if cmd_type == ":" then
+						return { "cmdline" }
+					end
+				end,
+				keymap = {
+					preset = "super-tab",
+				},
+				completion = {
+					menu = {
+						auto_show = true,
+					},
+				},
+			},
+			fuzzy = { implementation = "lua" },
+		},
+		opts_extend = { "sources.default" },
+	},
+
+	{
+		"nvimtools/none-ls.nvim",
+		opts = {},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		event = "VeryLazy",
+		config = function()
+			local null_ls = require("null-ls")
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.formatting.clang_format,
+				},
+			})
+		end,
+	},
+
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = "VeryLazy",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				rensure_isinstall = { "lua", "cpp" },
+			})
+		end,
+	},
+
+	{
+		"nvimdev/lspsaga.nvim",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons",
+		},
+		opts = {
+			ui = {
+				border = "none",
+				devicon = true,
+			},
+			code_action = {
+				keys = {
+					quit = "q",
+					exec = "<CR>",
+				},
+			},
+			lightbulb = {
+				enable = false,
+			},
+			finder = {
+				keys = {
+					toggle_or_open = "<CR>",
+				},
+			},
+			terminal = {
+				layout = "horizontal",
+				enter_on_open = true,
+				close_on_exit = true,
+			},
+		},
+		config = function(_, opts)
+			require("lspsaga").setup(opts)
+		end,
+		event = "VeryLazy",
+	},
+	{
+		"mfussenegger/nvim-dap",
+		event = "VeryLazy",
+	},
+}
---
+++ lua/plugins/function.lua
@@ -0,0 +1,67 @@
+return {
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		opts = {},
+		config = function()
+			local autopairs = require("nvim-autopairs")
+			autopairs.setup({
+				check_ts = true,
+			})
+		end,
+	},
+
+	{
+		"nvim-tree/nvim-tree.lua",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		lazy = false,
+		opts = {
+			disable_netrw = true,
+			hijack_netrw = true,
+		},
+		config = function(_, opts)
+			require("nvim-tree").setup(opts)
+		end,
+	},
+
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release &&"
+					.. "cmake --build build --config Release &&"
+					.. "cmake --install build --prefix build",
+			},
+		},
+		cmd = "Telescope",
+		opts = {
+            defaults = {
+                border =false,
+                borderchars =nil
+            },
+			extensions = {
+				fzf = {
+					fuzzy = true,
+					override_generic_sorter = true,
+					override_file_sorter = true,
+					case_mode = "smart_case",
+				},
+			},
+		},
+		config = function(_, opts)
+			local telescope = require("telescope")
+			telescope.setup(opts)
+			telescope.load_extension("fzf")
+		end,
+	},
+
+	{
+		"MagicDuck/grug-far.nvim",
+		cmd = "GrugFar",
+		opts = {},
+	},
+}
