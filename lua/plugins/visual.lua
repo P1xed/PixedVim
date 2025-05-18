9a22409ef9be1210402c31b2a0021860488959a2
_Pixed_ <140726085+P1xed@users.noreply.github.com>
2025-05-18 09:24:10 +0800

    Update visual.lua

--- lua/plugins/visual.lua
+++ lua/plugins/visual.lua
@@ -1,42 +1,46 @@
 return {
-    {
-        "nvim-lualine/lualine.nvim",
-        dependencies = {
-            "nvim-tree/nvim-web-devicons",
-        },
-        event = "VeryLazy",
-        opts = {
-            options = {
-                icons_enable = true,
-                theme = "auto",
-            },
-            sections = {
-                lualine_a = { "mode" },
-                lualine_b = { "diagnostics" },
-                lualine_c = { "filename" },
-                lualine_x = { "filetype" },
-                lualine_y = { "progress" },
-                lualine_z = { "location" },
-            },
-            extension = { "nvim-tree" },
-        },
-    },
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		event = "VeryLazy",
+		opts = {
+			options = {
+				icons_enable = true,
+				theme = "auto",
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "diagnostics" },
+				lualine_c = { "filename" },
+				lualine_x = { "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+			extension = { "nvim-tree" },
+		},
+	},
 
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        event = "VeryLazy",
-        main = "ibl",
-        opts = {},
-    },
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		event = "VeryLazy",
+		main = "ibl",
+		opts = {},
+	},
 
-    {
-        "akinsho/bufferline.nvim",
-        dependencies = {
-            "nvim-web-devicons",
-        },
-        opts = {},
-        event = "VeryLazy",
-    },
+	{
+		"akinsho/bufferline.nvim",
+		dependencies = {
+			"nvim-web-devicons",
+		},
+		opts = {},
+		event = "VeryLazy",
+	},
 
-    
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		event = "VeryLazy",
+		opts = {},
+	},
 }
