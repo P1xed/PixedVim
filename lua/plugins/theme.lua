39a4b4ba00ac172d65345e60e9ea54ffaa0f6098
_Pixed_ <140726085+P1xed@users.noreply.github.com>
2025-05-25 15:09:15 +0800

    Update theme.lua

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,18 +1,17 @@
-return
-{
-{   
-    "sainnhe/everforest",
-    --dependencies = {},
-    --event = "VeryLazy",
-    opts = {},
-    --keys = {},
-    --lazy = false,
-    config = function() --opts
-        --require("everforest").setup(opts)
-        vim.cmd("colorscheme everforest")
-    end
-},
-{"catppuccin/nvim"},
-{"olimorris/onedarkpro.nvim"},
-{"sainnhe/gruvbox-material"},
+return {
+    {
+        "sainnhe/everforest",
+        --dependencies = {},
+        --event = "VeryLazy",
+        opts = {},
+        --keys = {},
+        --lazy = false,
+        config = function() --opts
+            --require("everforest").setup(opts)
+            vim.cmd("colorscheme everforest")
+        end,
+    },
+    { "catppuccin/nvim" },
+    { "olimorris/onedarkpro.nvim" },
+    { "sainnhe/gruvbox-material" },
 }
